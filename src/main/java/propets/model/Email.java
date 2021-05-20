7e29ca069ef502bf49d14b9cd65cbe0bebd11edd
Constantine <linetski@gmail.com>
2021-05-20 19:47:10 +0300

    first commit

---
+++ src/main/java/propets/model/Email.java
@@ -0,0 +1,26 @@
+package propets.model;
+
+public class Email {
+	String subject; 
+	String body; 
+	String emailAdress;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getEmailAdress() {
+		return emailAdress;
+	}
+	public void setEmailAdress(String emailAdress) {
+		this.emailAdress = emailAdress;
+	}
+}
---
+++ src/main/java/propets/notificationService/NotificationService.java
@@ -0,0 +1,45 @@
+package propets.notificationService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import propets.model.Email;
+
+
+@Component
+public class NotificationService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+	private static final String EMAIL_TOPIC = "email";
+	
+	@Autowired
+    JavaMailSender emailSender;
+	
+	@KafkaListener(topics = EMAIL_TOPIC, groupId = "foo")
+	public void sendEmail(Email email) {
+		logger.info("email received from kafka: " + email);
+		SimpleMailMessage simpleMailMessage =constructEmail(email.getSubject(),email.getBody(),email.getEmailAdress());
+		emailSender.send(simpleMailMessage);
+	}
+	
+	private SimpleMailMessage constructEmail(String subject, String body, String email) {
+	    SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+	    simpleMailMessage.setSubject(subject);
+	    simpleMailMessage.setText(body);
+	    simpleMailMessage.setTo(email);
+	    return simpleMailMessage;
+	}
+	
+	public JavaMailSender getEmailSender() {
+		return emailSender;
+	}
+	
+	public void setEmailSender(JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+}
